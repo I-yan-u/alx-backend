f46a33a1dae5c111a2057c669e27cfed8cfdf84b
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-23 22:27:52 +0100

    Indexed hypermedia pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "0x00-pagination/Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Get hypermedia object with provided index
+        """
+        assert index < len(self.__indexed_dataset)
+        data_set = self.indexed_dataset()
+        start = index
+        while start not in data_set:
+            start += 1
+
+        data = []
+        for i in range(start, start + page_size):
+            data.append(data_set[i])
+            
+        return {
+            'index' : index,
+            'data': data,
+            'page_size': page_size,
+            'next_index': start + page_size
+        }
